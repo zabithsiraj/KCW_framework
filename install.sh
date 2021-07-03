f76a0d688adcb48c3dbb3d0529f8af576c415283
zabith siraj <85269952+zabithsiraj@users.noreply.github.com>
2021-07-03 08:33:52 -0400

    #

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget https://zabith.ml/contents/teamkcw.AppImage
+chmod 777 teamkcw.AppImage
+sudo mv $PWD/teamkcw.AppImage /usr/bin/keralacyberwarriors
+sudo mv $PWD/hackterminal /usr/bin/kcw
+
+echo "kcw framework successfully installed"
+echo "type kcw to start the framework at anytime anywhare!!"
+echo "WE ARE MALLUS && WE ARE GREAT"
+rm -rf intall.sh
+
